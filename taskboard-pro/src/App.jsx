1c042eedce267cd3b97cacd815cbc82ffff6719c
PauCCH <165844296+PauCCH@users.noreply.github.com>
2026-04-08 15:55:52 -0600

    TaskBoard.jsx parte 12

--- taskboard-pro/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import './App.css'
-
-function App() {
-  return <h1>TaskBoard Hooks Lab</h1>
-}
-
-export default App
---
+++ taskboard-pro/src/app/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TaskBoard } from "../features/tasks/components/taskBoard"
+
+function App() {
+  return (
+    <main>
+      <h1>TaskBoard Hooks Lab</h1>
+      <TaskBoard />
+    </main>
+  )
+}
+
+export default App
